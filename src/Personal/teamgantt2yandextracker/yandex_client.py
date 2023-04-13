96e81b6424046cbe26dd657c3bd6899a56a7a111
AlexeyStarovoitov <55379914+AlexeyStarovoitov@users.noreply.github.com>
2023-04-13 17:19:15 +0300

    debugged queue creation/deletion

--- src/Personal/teamgantt2yandextracker/yandex_client.py
+++ src/Personal/teamgantt2yandextracker/yandex_client.py
@@ -1,6 +1,7 @@
 from yandex_tracker_client import TrackerClient
 from enum import Enum
 import argparse
+import copy
 
 class TaskRealtionType(Enum):
     DEPENDENT_TASK_RELATION_TYPE = 1
@@ -23,17 +24,44 @@ class YandexTrackerClinet(TrackerClient):
         project = self.projects[project_id]
         if project:
             project.delete()
+    def get_workflows(self):
+        workflows = list(yandex_tracker.workflows.get_all())
+        new_workflows = []
+        for workflow in workflows:
+            new_workflow = dict(name=workflow.name, id=workflow.id, steps=[])
+            for step in workflow.steps:
+                new_step = {}
+                new_step['actions'] = []
+                for action in step['actions']:
+                    new_step['actions'].append(dict(id=action['id'], name=action['name']))
+                new_workflow['steps'].append(new_step)
+            new_workflows.append(new_workflow)
+        return new_workflows
+    def get_issue_types(self):
+        issue_types = list(self.issue_types.get_all())
+        new_issue_types = []
+        for issue_type in issue_types:
+            new_issue_type = dict(id=issue_type.id, key=issue_type.key, name=issue_type.name)
+            new_issue_types.append(new_issue_type)
+        return new_issue_types
+    def get_resolutions(self):
+        resolutions = list(yandex_tracker.resolutions.get_all())
+        new_resolutions = []
+        for resolution in resolutions:
+            new_resolution = dict(id=resolution.id, key=resolution.key, name=resolution.name)
+            new_resolutions.append(new_resolution)
+        return new_resolutions
     def create_queue(self, queue_name, key, type, priority, lead, issueTypesConfig):
         data = dict(name=queue_name, key = key, defaultType=type, defaultPriority=priority, lead=lead, issueTypesConfig=issueTypesConfig)
         resp = self.queues.create(params=None, **data)
         return resp.id
-    def _get_queue_id(self, queue_name):
+    def _get_queue_id(self, key):
         for queue in self.queues:
-            if queue.name == queue_name:
+            if queue.key == key:
                 return queue
         return None
-    def delete_queue(self, queue_name):
-        queue = self._get_queue_id(queue_name)
+    def delete_queue(self, key):
+        queue = self._get_queue_id(key)
         if queue:
             queue.delete()
     def get_issue_type_id(self, issue_name):
@@ -75,13 +103,6 @@ if __name__ == '__main__':
     for project in yandex_tracker.projects:
         print(project['name'])
     
-    print(type(yandex_tracker.queues))
-    print(dir(yandex_tracker.queues))
-    for queue in yandex_tracker.queues:
-        print(queue.key)
-
-    for issue_type in yandex_tracker.issue_types:
-        print(issue_type.id)
     # project methods check
     queues = list(yandex_tracker.queues.get_all())
     project_params = {}
@@ -94,23 +115,64 @@ if __name__ == '__main__':
     #queue methods check
     users = list(yandex_tracker.users.get_all())
     priorities = list(yandex_tracker.priorities.get_all())
-    issue_types = list(yandex_tracker.issue_types.get_all())
-    workflows = list(yandex_tracker.workflows.get_all())
-    resolutions = list(yandex_tracker.resolutions.get_all())
+    issue_types = yandex_tracker.get_issue_types()
+    resolutions = yandex_tracker.get_resolutions()
+    queues = list(yandex_tracker.queues.get_all())
+    
+    print("Queues:")
+    for queue in yandex_tracker.queues:
+        print(queue.id)
+        print(queue.key)
+        print(queue.name)
+        print(queue.lead)
+    
+    for priority in priorities:
+        print(priority.id)
+        print(priority.key)
+        print(priority.name)
+    '''
+    print("Resolutions:")
+    for resolution in resolutions:
+        print(resolution)
+    
+    print("Issue types:")
+    for issue_type in issue_types:
+        print(issue_type)
+
+    workflows = yandex_tracker.get_workflows()
+    print("Modified Workflows:")
+    for workflow in workflows:
+        print(workflow['id'])
+        print(workflow['name'])
+        print('Steps:')
+        for step in workflow['steps']:
+            print(step)
+
+
+    
+    '''
+    print("Users:")
+    for user in users:
+        print('')
+        print(user.uid)
+        print(user.login)
+    
     queue_params = {}
     queue_params["queue_name"] = "fake_garbage"
-    queue_params["key"] = "fake"
-    queue_params["lead"] = users[0].uid
-    queue_params["priority"] = priorities[0].id
+    queue_params["key"] = "FAKEGARBAGEEE"
+    queue_params["lead"] = "starovoitov-rep"
+    queue_params["priority"] = "normal"
     queue_params["type"] = "task"
     issueTypesConfig = []
+    resolutions_config = [res['key'] for res in resolutions]
     for issue_type in issue_types:
-        cur_type_config = dict(issueType = issue_type.key, workflow = workflows[0].id, resolutions = [res.key for res in resolutions])
+        cur_type_config = dict(issueType = issue_type['key'], workflow = "quickStartV2PresetWorkflow", resolutions = resolutions_config)
         issueTypesConfig.append(cur_type_config)
     queue_params["issueTypesConfig"] = issueTypesConfig
-    #queue_id = yandex_tracker.create_queue(**queue_params)
-    yandex_tracker.delete_queue(queue_params["queue_name"])
-
+    yandex_tracker.create_queue(**queue_params)
+    yandex_tracker.delete_queue(queue_params["key"])
+    
+    '''
     # task methods check
     task_params = {}
     task_params["task_name"]="Dummy Task"
@@ -119,4 +181,5 @@ if __name__ == '__main__':
     task_params["notes"] = "Just checking"
     #yandex_tracker.create_task(**task_params)
     #yandex_tracker.delete_task(task_params["task_name"])
+    '''
 
